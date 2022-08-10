f93c65318bfacda3d877e5e9d065add672240cc0
Quentin Rousselet <quentinr82@protonmail.com>
2022-08-10 10:40:50 +0200

    Change class main

---
+++ src/Forms/MainWindow.java
@@ -0,0 +1,48 @@
+package Forms;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+public class MainWindow extends JDialog {
+    private JPanel contentPane;
+    private JPanel LeftPanel;
+    private JPanel LeftPanelSouth;
+    private JLabel PersonalNoteLabel;
+    private JLabel FirstReadingLabel;
+    private JLabel LastReadingLabel;
+    private JPanel LeftPanelWest;
+    private JPanel BookPhotoPanel;
+    private JPanel BookExtractPanel;
+    private JButton ReadExtractBtn;
+    private JLabel TitleLabel;
+    private JPanel LeftPanelCenter;
+    private JPanel BookInfoGlobalPanel;
+    private JLabel AuthorLabel;
+    private JLabel NumberPageLabel;
+    private JLabel NoteLabel;
+    private JPanel BookSummaryPanel;
+    private JTextPane BookSummary;
+    private JLabel ExtractLabel;
+    private JPanel RightPanel;
+    private JPanel BookListPanel;
+    private JTable BookTableList;
+    private JPanel BookEditListPanel;
+    private JButton AddBookBtn;
+    private JButton FiltersBookBtn;
+    private JButton CancelFiltersBtn;
+    private JButton buttonOK;
+    private JButton buttonCancel;
+
+    public MainWindow() {
+        setContentPane(contentPane);
+        setModal(true);
+
+    }
+
+    public static void main(String[] args) {
+        MainWindow dialog = new MainWindow();
+        dialog.pack();
+        dialog.setVisible(true);
+        System.exit(0);
+    }
+}
